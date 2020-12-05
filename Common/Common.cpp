9bfb9d9eda4344adb4a80549b5e05dd0570794d7
mlogic1 <multilogic@outlook.com>
2020-12-05 15:22:40 +0100

    Added new functions to Common

--- Common/Common.cpp
+++ Common/Common.cpp
@@ -1,5 +1,6 @@
 #include "Common.h"
 #include <fstream>
+#include <sstream>
 
 namespace Common
 {
@@ -21,4 +22,36 @@ namespace Common
             throw "Error opening file";
         }
 	}
+
+    void LoadStringList(const std::string& fileName, std::vector<std::string>& list)
+    {
+        std::ifstream input(fileName.c_str(), std::ifstream::in);
+
+        std::string line;
+        if (input.is_open())
+        {
+            while (std::getline(input, line))
+            {
+                list.push_back(line);
+            }
+            input.close();
+        }
+        else
+        {
+            throw "Error opening file";
+        }
+    }
+
+    std::vector<std::string> SplitString(const std::string& str, const char& delimiter)
+    {
+        std::vector<std::string> result;
+        std::string token;
+        std::istringstream stream(str);
+
+        while (std::getline(stream, token, delimiter))
+        {
+            result.push_back(token);
+        }
+        return result;
+    }
 }
--- Common/Common.h
+++ Common/Common.h
@@ -6,4 +6,6 @@
 namespace Common
 {
 	void LoadIntegerList(const std::string& fileName, std::vector<int>& list);
+	void LoadStringList(const std::string& fileName, std::vector<std::string>& list);
+	std::vector<std::string> SplitString(const std::string& str, const char& delimiter);
 }
